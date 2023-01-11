1de413c177da1f21cc51fc7616f8bdbca361ef14
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2023-01-11 14:45:25 +0100

    ✨ completed: Flutter Paystack

--- lib/main.dart
+++ lib/main.dart
@@ -1,186 +1,186 @@
-import 'package:flutter/material.dart';
-import 'package:paystack/payment/paystack_payment.dart';
+// import 'package:flutter/material.dart';
+// import 'package:paystack/payment/paystack_payment.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter PayStack',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Paystack Payment'),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter PayStack',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: 'Flutter Paystack Payment'),
+//     );
+//   }
+// }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
 
-  final String title;
+//   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  //creating variables  //variable to hold the price selected from the GridView
-  int? selectedIndex;
+// class _MyHomePageState extends State<MyHomePage> {
+//   //creating variables  //variable to hold the price selected from the GridView
+//   int? selectedIndex;
 
-  //User Email to be sent to paystack (Dummy)
-  String? email = "clementpeter2000@gmail.com";
+//   //User Email to be sent to paystack (Dummy)
+//   String? email = "clementpeter2000@gmail.com";
 
-  //variable to hold the price selected price from the GridView
-  int? price = 0;
+//   //variable to hold the price selected price from the GridView
+//   int? price = 0;
 
-  //Map containing list of shop items in our app
-  final List<Map<String, Object>> shopItems = [
-    {'price': 1000, "item": "Milo"},
-    {'price': 2000, "item": "Milk"},
-    {'price': 3000, "item": "Parfait"},
-    {'price': 4000, "item": "Kellogs"},
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0),
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 50),
-              const Center(
-                child: Text(
-                  'Purchase available items',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    //letterSpacing: 5,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: GridView(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 10,
-                        crossAxisSpacing: 5,
-                      ),
-                      children: List.generate(
-                        shopItems.length,
-                        (index) {
-                          final data = shopItems[index];
-                          return GestureDetector(
-                            onTap: () {
-                              setState(
-                                () {
-                                  selectedIndex =
-                                      index; //identify the grid selected to get the item and price
-                                  price = data["price"] as int;
-                                  //print("Grid view Selected price::::::::::::::::$price");
-                                },
-                              );
-                            },
-                            child: Card(
-                              elevation: 5,
-                              shadowColor: Colors.green,
-                              child: Container(
-                                color: selectedIndex == null
-                                    ? null
-                                    : selectedIndex == index
-                                        ? const Color.fromARGB(182, 76, 175, 79)
-                                        : null,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      'Price: N${data['price']}',
-                                      style: const TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    Text(
-                                      'Items: ${data['item']}',
-                                      style: const TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      )),
-                ),
-              ),
-              //Make payment Button
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: GestureDetector(
-                  onTap: () {
-                    //Trigger paystack payment, if an item is selected
-                    if (selectedIndex == null) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("Please select an item"),
-                        ),
-                      );
-                    } else {
-                      //make payment via paystack
-                      print(
-                          "Grid view Selected price:::::::::::::::::::$price");
-                      MakePayment(ctx: context, price: price, email: email)
-                          .chargeCardAndMakePayment();
-                    }
-                  },
-                  //make payment button
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    height: 50,
-                    width: MediaQuery.of(context).size.width,
-                    alignment: Alignment.center,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.security,
-                          color: Colors.white,
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          "Make Payment",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 50)
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   //Map containing list of shop items in our app
+//   final List<Map<String, Object>> shopItems = [
+//     {'price': 1000, "item": "Milo"},
+//     {'price': 2000, "item": "Milk"},
+//     {'price': 3000, "item": "Parfait"},
+//     {'price': 4000, "item": "Kellogs"},
+//   ];
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//         centerTitle: true,
+//       ),
+//       body: Center(
+//         child: Padding(
+//           padding: const EdgeInsets.symmetric(vertical: 10.0),
+//           child: Column(
+//             children: <Widget>[
+//               const SizedBox(height: 50),
+//               const Center(
+//                 child: Text(
+//                   'Purchase available items',
+//                   style: TextStyle(
+//                     fontSize: 30,
+//                     fontWeight: FontWeight.bold,
+//                     //letterSpacing: 5,
+//                   ),
+//                 ),
+//               ),
+//               const SizedBox(height: 30),
+//               Expanded(
+//                 child: Padding(
+//                   padding: const EdgeInsets.symmetric(horizontal: 10.0),
+//                   child: GridView(
+//                       gridDelegate:
+//                           const SliverGridDelegateWithFixedCrossAxisCount(
+//                         crossAxisCount: 2,
+//                         mainAxisSpacing: 10,
+//                         crossAxisSpacing: 5,
+//                       ),
+//                       children: List.generate(
+//                         shopItems.length,
+//                         (index) {
+//                           final data = shopItems[index];
+//                           return GestureDetector(
+//                             onTap: () {
+//                               setState(
+//                                 () {
+//                                   selectedIndex =
+//                                       index; //identify the grid selected to get the item and price
+//                                   price = data["price"] as int;
+//                                   //print("Grid view Selected price::::::::::::::::$price");
+//                                 },
+//                               );
+//                             },
+//                             child: Card(
+//                               elevation: 5,
+//                               shadowColor: Colors.green,
+//                               child: Container(
+//                                 color: selectedIndex == null
+//                                     ? null
+//                                     : selectedIndex == index
+//                                         ? const Color.fromARGB(182, 76, 175, 79)
+//                                         : null,
+//                                 child: Column(
+//                                   mainAxisAlignment: MainAxisAlignment.center,
+//                                   children: [
+//                                     Text(
+//                                       'Price: N${data['price']}',
+//                                       style: const TextStyle(
+//                                         fontSize: 20,
+//                                         fontWeight: FontWeight.bold,
+//                                       ),
+//                                     ),
+//                                     Text(
+//                                       'Items: ${data['item']}',
+//                                       style: const TextStyle(
+//                                         fontSize: 20,
+//                                         fontWeight: FontWeight.bold,
+//                                       ),
+//                                     ),
+//                                   ],
+//                                 ),
+//                               ),
+//                             ),
+//                           );
+//                         },
+//                       )),
+//                 ),
+//               ),
+//               //Make payment Button
+//               Padding(
+//                 padding: const EdgeInsets.symmetric(horizontal: 30.0),
+//                 child: GestureDetector(
+//                   onTap: () {
+//                     //Trigger paystack payment, if an item is selected
+//                     if (selectedIndex == null) {
+//                       ScaffoldMessenger.of(context).showSnackBar(
+//                         const SnackBar(
+//                           content: Text("Please select an item"),
+//                         ),
+//                       );
+//                     } else {
+//                       //make payment via paystack
+//                       print(
+//                           "Grid view Selected price:::::::::::::::::::$price");
+//                       MakePayment(ctx: context, price: price, email: email)
+//                           .chargeCardAndMakePayment();
+//                     }
+//                   },
+//                   //make payment button
+//                   child: Container(
+//                     decoration: BoxDecoration(
+//                       color: Colors.green,
+//                       borderRadius: BorderRadius.circular(5),
+//                     ),
+//                     height: 50,
+//                     width: MediaQuery.of(context).size.width,
+//                     alignment: Alignment.center,
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: const [
+//                         Icon(
+//                           Icons.security,
+//                           color: Colors.white,
+//                         ),
+//                         SizedBox(width: 10),
+//                         Text(
+//                           "Make Payment",
+//                           style: TextStyle(color: Colors.white),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//               const SizedBox(height: 50)
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/payment/paystack_payment.dart
+++ lib/payment/paystack_payment.dart
@@ -1,77 +1,78 @@
-import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:flutter_paystack/flutter_paystack.dart';
-import 'package:paystack/constant/key.dart';
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_paystack/flutter_paystack.dart';
+// import 'package:paystack/constant/key.dart';
 
-class MakePayment {
-  MakePayment({this.ctx, this.price, this.email});
+// class MakePayment {
+//   MakePayment({this.ctx, this.price, this.email});
 
-  //variables to be passed to pauystack plugin
-  BuildContext? ctx;
-  int? price;
-  String? email;
+//   //variables to be passed to pauystack plugin
+//   BuildContext? ctx;
+//   int? price;
+//   String? email;
 
-  //1-Initialize paystack plugin
-  PaystackPlugin paystackPlugin = PaystackPlugin();
+//   //1-Initialize paystack plugin
+//   PaystackPlugin paystackPlugin = PaystackPlugin();
 
-  //2- private method to initialize plugin with the public key
-  Future _initialisePlugin() async {
-    await paystackPlugin.initialize(publicKey: ConstantKey.PAYSTACK_KEY);
-  }
+//   //2- private method to initialize plugin with the public key
+//   Future _initialisePlugin() async {
+//     await paystackPlugin.initialize(publicKey: ConstantKey.PAYSTACK_KEY);
+//   }
 
-  //4-Private method to get reference for paystack
-  String _getReference() {
-    String platform;
-    if (Platform.isIOS) {
-      platform = "IOS";
-    } else {
-      platform = "Android";
-    }
-    return "Payment made from $platform ${DateTime.now().millisecondsSinceEpoch}";
-  }
+//   //4-Private method to get reference for paystack
+//   String _getReference() {
+//     String platform;
+//     if (Platform.isIOS) {
+//       platform = "IOS";
+//     } else {
+//       platform = "Android";
+//     }
+//     return "Payment made from $platform ${DateTime.now().millisecondsSinceEpoch}";
+//   }
 
-  //5- Get Prebuilt UI for card payment
-  PaymentCard _getCardUI() {
-    return PaymentCard(number: "", cvc: "", expiryMonth: 0, expiryYear: 0);
-  }
+//   //5- Get Prebuilt UI for card payment
+//   PaymentCard _getCardUI() {
+//     return PaymentCard(number: "", cvc: "", expiryMonth: 0, expiryYear: 0);
+//   }
 
-  //3-method of paying by card - using "Check Out Method"
-  chargeCardAndMakePayment() {
-    _initialisePlugin().then((_) async {
-      Charge charge = Charge()
-        ..amount =
-            price! * 100 //rounding the price to a whole naira and removing kobo
-        ..email = email!
-        ..reference = _getReference()
-        //..accessCode ="Zenith bank" //used for "CheckOutMethod.selectable" check out to show bank names
-        ..putCustomField("Charged from", "Velobox")
-        ..card = _getCardUI();
+//   //3-method of paying by card - using "Check Out Method"
+//   chargeCardAndMakePayment() {
+//     _initialisePlugin().then((_) async {
+//       Charge charge = Charge()
+//         ..amount =
+//             price! * 100 //rounding the price to a whole naira and removing kobo
+//         ..email = email!
+//         ..reference = _getReference()
+//         //..accessCode ="Zenith bank" //used for "CheckOutMethod.selectable" check out to show bank names
+//         ..putCustomField("Charged from", "Velobox")
+//         ..card = _getCardUI();
 
-      final response = await paystackPlugin.checkout(
-        ctx!,
-        charge: charge,
-        method: CheckoutMethod.card, //Comment if your'e using access code
-        logo: const FlutterLogo(),
-      );
-      print("RESPONSE:::::::::::::::::::::::::::::$response");
-      //LAST- Function to perform if the trasaction is a succes or failure
-      if (response.status == true) {
-        ScaffoldMessenger.of(ctx!).showSnackBar(
-          const SnackBar(
-            backgroundColor: Colors.green,
-            duration: Duration(seconds: 3),
-            content: Text("Transaction Successful"),
-          ),
-        );
-      } else {
-        ScaffoldMessenger.of(ctx!).showSnackBar(
-          const SnackBar(
-            backgroundColor: Colors.red,
-            duration: Duration(seconds: 3),
-            content: Text("Transaction Cancelled"),
-          ),
-        );
-      }
-    });
-  }
-}
+//       final response = await paystackPlugin.checkout(
+//         ctx!,
+//         charge: charge,
+//         method: CheckoutMethod.card, //Comment if your'e using access code
+//         logo: const FlutterLogo(),
+//       );
+//       print("RESPONSE:::::::::::::::::::::::::::::$response");
+
+//       //LAST- Function to perform if the trasaction is a succes or failure
+//       if (response.status == true) {
+//         ScaffoldMessenger.of(ctx!).showSnackBar(
+//           const SnackBar(
+//             backgroundColor: Colors.green,
+//             duration: Duration(seconds: 3),
+//             content: Text("Transaction Successful"),
+//           ),
+//         );
+//       } else {
+//         ScaffoldMessenger.of(ctx!).showSnackBar(
+//           const SnackBar(
+//             backgroundColor: Colors.red,
+//             duration: Duration(seconds: 3),
+//             content: Text("Transaction Cancelled"),
+//           ),
+//         );
+//       }
+//     });
+//   }
+// }
